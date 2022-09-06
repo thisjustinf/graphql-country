48c5b98d687d44728edefa47e27c4c691bcd64bd
Justin Fagan <jbfagan7@gmail.com>
2022-09-05 23:33:38 -0400

    fix: extended Country type with new fields

--- src/types/Country.ts
+++ src/types/Country.ts
@@ -1,4 +1,15 @@
+import Continent from "./Continent";
+import Language from "./Language";
+
 export default interface Country {
   code: string;
   name: string;
+  currency?: string;
+  capital?: string;
+  native: string;
+  phone: string;
+  continent: Continent;
+  languages: Language[];
+  emoji: string;
+  emojiU: string;
 }
