5abaa1db5dec250667b3890d513b591ea0ff5af1
Justin Fagan <jbfagan7@gmail.com>
2022-09-07 11:25:14 -0400

    feat: added page routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import Countries from "./components/Countries";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Continent from "./components/Continent/Continent";
+import Continents from "./components/Continent/Continents";
+import Countries from "./components/Country/Countries";
+import Country from "./components/Country/Country";
+import Home from "./components/Home/Home";
+import Language from "./components/Language/Language";
+import Languages from "./components/Language/Languages";
 
 function App() {
   return (
-    <div className="App">
-      <Countries />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/continents" element={<Continents />} />
+        <Route path="/continents/:code" element={<Continent />} />
+        <Route path="/countries" element={<Countries />} />
+        <Route path="/countries/:code" element={<Country />} />
+        <Route path="/languages/" element={<Languages />} />
+        <Route path="/languages/:code" element={<Language />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
