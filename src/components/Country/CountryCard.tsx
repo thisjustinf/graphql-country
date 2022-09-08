a2efd256723be44de551f0436e05c616dc02fc23
Justin Fagan <jbfagan7@gmail.com>
2022-09-08 07:43:52 -0400

    feat: created boilerplate card and not found components

---
+++ src/components/Country/CountryCard.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React, { FC } from "react";
+import Country from "../../types/Country";
+
+const CountryCard: FC<Country> = ({}) => {
+  const property = {};
+  return (
+    <Box maxW="md" maxH="md" overflow="hidden">
+      CountryCard
+    </Box>
+  );
+};
+
+export default CountryCard;
