7359313279c15264ac26660321cd0587e4584373
Justin Fagan <jbfagan7@gmail.com>
2022-09-12 11:51:39 -0400

    fix: removing redundant pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Continent from "./components/Continent/Continent";
 import Country from "./components/Country/Country";
 import Home from "./components/Home/Home";
-import Language from "./components/Language/Language";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/continent/:code" element={<Continent />} />
         <Route path="/country/:code" element={<Country />} />
-        <Route path="/language/:code" element={<Language />} />
       </Routes>
     </BrowserRouter>
   );
